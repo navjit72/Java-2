3903d37193162061d8c7480f2e796d2fad5de550
Navjit Kaur <n01267930@humbermail.ca>
2018-07-13 19:27:52 -0400

    Assignment 3 done

--- Week10Lab/src/ActorMgr.java
+++ Week10Lab/src/ActorMgr.java
@@ -35,10 +35,10 @@ public class ActorMgr {
             actor.setFirst_name(resultSet.getString(2));
             actor.setLast_name(resultSet.getString(3));
             actor.setLast_update(resultSet.getTimestamp(4));
+            return actor;
         }catch(SQLException ex){
-            ex.printStackTrace();
         }
-        return actor;
+        return null;
     }
 
     public static boolean insertRecord(Actor actor){
@@ -49,46 +49,46 @@ public class ActorMgr {
             preparedStatement.setString(1,actor.getFirst_name());
             preparedStatement.setString(2,actor.getLast_name());
             result=preparedStatement.executeUpdate();
-            if(result<=0)
-                return false;
+            if(result>0)
+                return true;
 
         }catch(SQLException ex){
             ex.printStackTrace();
         }
-        return true;
+        return false;
     }
 
     public static boolean updateRecord(Actor actor){
         int result=0;
         try{
             connection = DriverManager.getConnection("jdbc:mysql://localhost/sakila", "root", "navjithumber");
-            preparedStatement = connection.prepareStatement("update table actor set first_name=?, last_name=? where actor_id=?");
+            preparedStatement = connection.prepareStatement("update actor set first_name=?, last_name=? where actor_id=?");
             preparedStatement.setString(1,actor.getFirst_name());
             preparedStatement.setString(2,actor.getLast_name());
             preparedStatement.setInt(3,actor.getActor_id());
             result=preparedStatement.executeUpdate();
-            if(result<=0)
-                return false;
+            if(result>0)
+                return true;
 
         }catch(SQLException ex){
             ex.printStackTrace();
         }
-        return true;
+        return false;
     }
 
     public static boolean deleteRecord(int actor_id){
         int result=0;
         try{
             connection = DriverManager.getConnection("jdbc:mysql://localhost/sakila", "root", "navjithumber");
-            preparedStatement = connection.prepareStatement("delete from actor set where actor_id=?");
+            preparedStatement = connection.prepareStatement("delete from actor where actor_id=?");
             preparedStatement.setInt(1,actor_id);
             result=preparedStatement.executeUpdate();
-            if(result<=0)
-                return false;
+            if(result>0)
+                return true;
 
         }catch(SQLException ex){
             ex.printStackTrace();
         }
-        return true;
+        return false;
     }
 }
--- Week10Lab/src/Main.java
+++ Week10Lab/src/Main.java
@@ -9,14 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Actor actor=new Actor();
+        Actor actor = new Actor();
         try {
 
             while (true) {
                 int option = 1;
                 System.out.println("Actor Table : ");
-                System.out.println("1- Display all Rows | 2- Get a row | 3- Add a new row | 4- Update an existing row " +
-                        "| 5- Delete a row | 6-Exit the application");
+                System.out.println("1- Display all Rows | 2- Get a row | 3- Add a new row | 4- Update an existing row "
+                        + "| 5- Delete a row | 6-Exit the application");
                 System.out.println("Select an option : ");
                 option = input.nextInt();
                 switch (option) {
@@ -26,37 +26,39 @@ public class Main {
                     case 2: {
                         System.out.println("Enter the actor id : ");
                         actor = ActorMgr.getRecord(input.nextInt());
-                        System.out.println("Id : " + actor.getActor_id() + "\nFirstName : " + actor.getFirst_name() +
-                                "\nLastName : " + actor.getLast_name() + "\nLast update : " + actor.getLast_update());
+                        if (actor != null) {
+                            System.out.println("Id : " + actor.getActor_id() + "\nFirstName : " + actor.getFirst_name()
+                                    + "\nLastName : " + actor.getLast_name() + "\nLast update : " + actor.getLast_update());
+                        } else {
+                            System.out.println("No such actor found");
+                        }
                         break;
                     }
                     case 3: {
                         System.out.println("Enter the details of actor in the order of firstname, lastname : ");
                         actor.setFirst_name(input.next());
                         actor.setLast_name(input.next());
-                        if(ActorMgr.insertRecord(actor))
+                        if (ActorMgr.insertRecord(actor))
                             System.out.println("Row inserted successfully");
                         else
                             System.out.println("Row insertion failed");
                         break;
                     }
-                    case 4:
-                    {
+                    case 4: {
                         System.out.println("Enter the actor id you want to update : ");
                         actor.setActor_id(input.nextInt());
                         System.out.println("Enter the details of actor in the order of firstname, lastname : ");
                         actor.setFirst_name(input.next());
                         actor.setLast_name(input.next());
-                        if(ActorMgr.updateRecord(actor))
+                        if (ActorMgr.updateRecord(actor))
                             System.out.println("Row updated successfully");
                         else
                             System.out.println("Row updation failed");
                         break;
                     }
-                    case 5:
-                    {
+                    case 5: {
                         System.out.println("Enter the actor id you want to delete : ");
-                        if(ActorMgr.deleteRecord(input.nextInt()))
+                        if (ActorMgr.deleteRecord(input.nextInt()))
                             System.out.println("Row deleted successfully");
                         else
                             System.out.println("Row deletion failed");
@@ -64,10 +66,16 @@ public class Main {
                     }
                     case 6:
                         System.exit(0);
+
+                    default: {
+                        System.out.println("Invalid option");
+                        break;
+                    }
                 }
 
+
             }
-        }catch (NumberFormatException ex) {
+        } catch (NumberFormatException ex) {
             System.out.println("Enter an integer as option value");
         }
     }
